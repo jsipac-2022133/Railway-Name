4140ed4c2bfbe422daf9c04a0a1c75246a6f4062
jsipac-2022133 <jsipac-2022133@kinal.edu.gt>
2024-10-09 08:27:23 -0600

    Se implementó microservicio que devuelve mi nombre

---
+++ raylway/src/main/java/com/jsipac/raylway/RaylwayApplication.java
@@ -0,0 +1,14 @@
+package com.jsipac.raylway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaylwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaylwayApplication.class, args);
+		System.out.println("James Brian Sipac Sipac \uD83D\uDC68\u200D\uD83D\uDCBB");
+	}
+
+}
